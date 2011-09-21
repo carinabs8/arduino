e80dac7a2afc5cfde4e49412f7cca251dbebff08
carinabs8 <carinabs8@gmail.com>
2011-09-21 19:32:02 -0400

    fix

--- arduino/banco.rb
+++ arduino/banco.rb
@@ -1,7 +1,13 @@
 class Banco
   require 'sequel'
+  
   def initialize(adapter, host, database, user, password)
     db = Sequel.connect(:adapter => adapter, :host => host, :database =>database, :user => user, :password => password)
   end
+   #DB.create_table :arduino_vagas do
+  #  primary_key :id
+  #  String :name
+  #  String :status
+  #end
   
 end
--- arduino/vaga.rb
+++ arduino/vaga.rb
@@ -1,16 +1,10 @@
 class Vaga
   require 'rubygems'
   require 'serialport'
-  require 'sequel'
+  require 'banco'
   
-  DB = Sequel.connect(:adapter => "postgres", :host => "localhost", :database => "projet_final_development", :user => "postgres", :password => "starfaty")
-  #bd = Banco.new("postgres", "localhost", "projeto_final_development", "postgres", "starfaty")
-  
-  #DB.create_table :arduino_vagas do
-  #  primary_key :id
-  #  String :name
-  #  String :status
-  #end
+  bd = Banco.new("postgres", "localhost", "projeto_final_development", "postgres", "starfaty")
+ 
   sp = SerialPort.new("/dev/ttyUSB3", 9600, 8, 1, SerialPort::NONE)
 i = 0
   while true do
