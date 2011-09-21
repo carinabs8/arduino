f22edd4d68afb5b552a2bc9fc13c49cce273519f
carinabs8 <carinabs8@gmail.com>
2011-09-21 01:20:07 -0400

    test

--- arduino/vaga.rb
+++ arduino/vaga.rb
@@ -3,12 +3,21 @@ class Vaga
   require 'serialport'
   require 'sequel'
   
-  db = Sequel.connect(:adapter => "postgres", :host => "localhost", :database => "projeto_final_development", :user => "postgres", :password => "starfaty")
+  DB = Sequel.connect(:adapter => "postgres", :host => "localhost", :database => "projet_final_development", :user => "postgres", :password => "starfaty")
   #bd = Banco.new("postgres", "localhost", "projeto_final_development", "postgres", "starfaty")
- 
-  sp = SerialPort.new("/dev/ttyUSB4", 9600, 8, 1, SerialPort::NONE)
-
+  
+  #DB.create_table :arduino_vagas do
+  #  primary_key :id
+  #  String :name
+  #  String :status
+  #end
+  sp = SerialPort.new("/dev/ttyUSB3", 9600, 8, 1, SerialPort::NONE)
+i = 0
   while true do
     printf("%s", sp.getc)
+    unless sp.getc.nil? || sp.getc == ""
+      printf("Estaos ai %i", i)
+      i= i + 1
+    end
   end
 end
