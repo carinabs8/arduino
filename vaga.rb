cf31f1b9b8f8c860f4899ef49b3100512d0ffd1f
carinabs8 <carinabs8@gmail.com>
2011-09-25 02:10:34 -0400

    updates

--- vaga.rb
+++ vaga.rb
@@ -7,12 +7,9 @@ class Vaga
   bd = Banco.new("postgres", "localhost", "projeto_final_development", "postgres", "starfaty")
  
   sp = SerialPort.new("/dev/ttyUSB3", 9600, 8, 1, SerialPort::NONE)
-i = 0
   while true do
-    printf("%s", sp.getc)
-    unless sp.getc.nil? || sp.getc == ""
-      printf("Estaos ai %i", i)
-      i= i + 1
+    unless sp.gets.nil? || sp.gets == ""
+      printf("%s", sp.getc)
     end
   end
 end
