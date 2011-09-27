95b35bd886092afbbba3528e2c48f245e6c6afa0
carinabs8 <carinabs8@gmail.com>
2011-09-27 02:19:16 -0400

    states of vaga

--- banco.rb
+++ banco.rb
@@ -4,10 +4,5 @@ class Banco
   def initialize(adapter, host, database, user, password)
     db = Sequel.connect(:adapter => adapter, :host => host, :database =>database, :user => user, :password => password)
   end
-   #DB.create_table :arduino_vagas do
-  #  primary_key :id
-  #  String :name
-  #  String :status
-  #end
   
 end
--- vaga.rb
+++ vaga.rb
@@ -5,15 +5,20 @@ class Vaga
   
   
   bd = Banco.new("postgres", "localhost", "projet_final_development", "postgres", "starfaty")
-  sp = SerialPort.new("/dev/ttyUSB0", 9600, 8, 1, SerialPort::NONE)
+  sp = SerialPort.new("/dev/ttyUSB3", 9600, 8, 1, SerialPort::NONE)
   
   AVAILABLE   = "0"
   RESTRICTED  = "1"
   BUSY        = "2"
+  
   while true do
-	msg = sp.gets
+    msg = sp.gets
     unless msg.nil?
-      puts msg
+      cod_arduino = msg.split(":")
+      st = db[:status_controlls].filter("cod_arduino = #{cod_arduino[0]}").order(:id).last
+      if st[:cod_arduino] !=  RESTRICTED
+        puts msg
+      end
     end
     sleep(1)
    end
