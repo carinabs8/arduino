ac144b2b12f04c2c0a652336b55fa022b9e931a1
carinabs8 <carinabs8@gmail.com>
2011-10-10 00:49:54 -0400

    salva status da vaga quando insere ou atualiza o status da vaga

--- vaga.rb
+++ vaga.rb
@@ -4,7 +4,7 @@ class Vaga
   require 'sequel'
   
   db = Sequel.connect(:adapter => "postgres", :host => "localhost", :database => "projet_final_development", :user => "postgres", :password => "starfaty")
-  sp = SerialPort.new("/dev/ttyUSB1", 9600, 8, 1, SerialPort::NONE)
+  sp = SerialPort.new("/dev/ttyUSB3", 9600, 8, 1, SerialPort::NONE)
   
   AVAILABLE   = "0"
   RESTRICTED  = "1"
@@ -12,30 +12,39 @@ class Vaga
   
   def self.update_status_controll(cod_arduino, db)
     db[:status_controlls].filter(:cod_arduino => cod_arduino, :time_end => nil).update(:time_end => Time.now)
+    save_vaga_status(cod_arduino, db, AVAILABLE)
   end
-  def self.save_status_controll(status, cod_arduino, db)
-    db[:status_controlls].insert(:status => status, :timebegin => Time.now, :cod_arduino => cod_arduino)
+  
+  def self.save_status_controll(cod_arduino, db)
+    db[:status_controlls].insert(:timebegin => Time.now, :cod_arduino => cod_arduino)
+    save_vaga_status(cod_arduino, db, BUSY)
   end
-
+  
+  def self.save_vaga_status(cod_arduino, db, status)
+    vaga            = db[:vagas].filter(:cod_arduino => cod_arduino).order(:id).last
+    status_controll = db[:status_controlls].filter(:cod_arduino => cod_arduino).order(:id).last
+    
+    db[:vaga_status].insert(:vaga_id => vaga[:id], :status_controll_id => status_controll[:id], :status => status)
+  end
   while true do
     msg = sp.gets
     unless msg.nil?
       cod_arduino = msg.split(":")
-      st = db[:status_controlls].filter(:cod_arduino => cod_arduino[0]).order(:id).last
       puts cod_arduino
+      st = db[:status_controlls].filter(:cod_arduino => cod_arduino[0]).order(:id).last
       if st.nil?
-        save_status_controll(BUSY, cod_arduino[0], db)
+        save_status_controll(cod_arduino[0], db)
       elsif !st.nil?
         if st[:status] !=  RESTRICTED
           if st[:time_end].nil?
             self.update_status_controll(cod_arduino[0], db)
             if cod_arduino[1] == BUSY
-              save_status_controll(BUSY, cod_arduino[0], db)
+              save_status_controll(cod_arduino[0], db)
             end
           end
           
           if !st[:time_end].nil? and cod_arduino[1] == BUSY
-            save_status_controll(BUSY, cod_arduino[0], db)
+            save_status_controll(cod_arduino[0], db)
           end
         end
       end
