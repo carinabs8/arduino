adbcc3032fe248bf654412c0c67c50274d6c702e
carinabs8 <carinabs8@gmail.com>
2011-10-10 01:23:19 -0400

    só atualiza a tabela de statusControll e insere dados na tabela de vaga status se existir uma vaga pertinente ao código do arduino

--- vaga.rb
+++ vaga.rb
@@ -10,14 +10,23 @@ class Vaga
   RESTRICTED  = "1"
   BUSY        = "2"
   
+  def self.exist_vaga?(cod_arduino)
+    vaga            = @db[:vagas].filter(:cod_arduino => cod_arduino).order(:id).last
+    puts vaga
+    return true if !vaga.nil?
+  end
   def self.update_status_controll(cod_arduino)
-    @db[:status_controlls].filter(:cod_arduino => cod_arduino, :time_end => nil).update(:time_end => Time.now)
-    save_vaga_status(cod_arduino, AVAILABLE)
+    if exist_vaga?(cod_arduino)
+      @db[:status_controlls].filter(:cod_arduino => cod_arduino, :time_end => nil).update(:time_end => Time.now)
+      save_vaga_status(cod_arduino, AVAILABLE)
+    end
   end
   
   def self.save_status_controll(cod_arduino)
-    @db[:status_controlls].insert(:timebegin => Time.now, :cod_arduino => cod_arduino)
-    save_vaga_status(cod_arduino, BUSY)
+    if exist_vaga?(cod_arduino)
+      @db[:status_controlls].insert(:timebegin => Time.now, :cod_arduino => cod_arduino)
+      save_vaga_status(cod_arduino, BUSY)
+    end
   end
   
   def self.save_vaga_status(cod_arduino, status)
